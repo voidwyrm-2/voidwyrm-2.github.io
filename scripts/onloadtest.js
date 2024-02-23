75becd32105a2d9a7b709fcc21966ce27b08f03a
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2024-02-23 08:56:50 -0500

    wwfwefwf

---
+++ scripts/onloadtest.js
@@ -0,0 +1,21 @@
+const log = document.querySelector(".event-log-contents");
+const reload = document.querySelector("#reload");
+
+reload.addEventListener("click", () => {
+  log.textContent = "";
+  setTimeout(() => {
+    window.location.reload(true);
+  }, 200);
+});
+
+window.addEventListener("load", (event) => {
+  log.textContent += "load\n";
+});
+
+document.addEventListener("readystatechange", (event) => {
+  log.textContent += `readystate: ${document.readyState}\n`;
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  log.textContent += `DOMContentLoaded\n`;
+});
